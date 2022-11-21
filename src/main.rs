bbb942ad6efaa7a9b09313969c43a130c76af777
Cameron Kientz <cameron.kientz@fishtech.group>
2022-11-21 14:27:15 -0600

    chore: yay for ? error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::error::Error;
+use home;
 use serde_yaml;
 use std::env;
-use std::process::Command;
-use std::fs;
+use std::error::Error;
 use std::fmt;
-use home;
+use std::fs;
+use std::process::Command;
 
 struct Config {
     notes_directory: Box<String>,
@@ -24,17 +24,10 @@ impl fmt::Display for MyError {
 impl Error for MyError {}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    
-    let config_file = match find_config() {
-        Ok(value) => value,
-        Err(e) => return Err(e),
-    };
+    let config_file = find_config()?;
 
-    let config: Config = match unmarshall_yaml(&config_file) {
-        Ok(value) => value,
-        Err(e) => return Err(e),
-    };
-    
+    let config: Config = unmarshall_yaml(&config_file)?;
+
     // args array includes executable itself
     let args: Vec<String> = env::args().collect();
     return match args.len() {
@@ -46,7 +39,6 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn find_config() -> Result<String, Box<dyn Error>> {
-
     let home_path_buf = match home::home_dir() {
         Some(pathbuf) => pathbuf,
         None => return Err(Box::new(MyError("Could not get home directory".into()))),
@@ -59,19 +51,13 @@ fn find_config() -> Result<String, Box<dyn Error>> {
 
     let config_file = format!("{}/.note-rs/config.yaml", home);
 
-   Ok(String::from(config_file)) 
+    Ok(String::from(config_file))
 }
 
 fn unmarshall_yaml(config_file_path: &str) -> Result<Config, Box<dyn Error>> {
-    let config_file = match std::fs::File::open(config_file_path) {
-        Ok(value) => value,
-        Err(e) => return Err(Box::new(e)),
-    };
+    let config_file = std::fs::File::open(config_file_path)?;
 
-    let config_yaml: serde_yaml::Value = match serde_yaml::from_reader(config_file) {
-        Ok(value) => value,
-        Err(e) => return Err(Box::new(e)),
-    };
+    let config_yaml: serde_yaml::Value = serde_yaml::from_reader(config_file)?;
 
     let config = Config {
         notes_directory: match config_yaml["notes_directory"].as_str() {
@@ -88,14 +74,17 @@ fn unmarshall_yaml(config_file_path: &str) -> Result<Config, Box<dyn Error>> {
     Ok(config)
 }
 
-
 fn match_singleton_command(arg: &str, config: &Config) -> Result<(), Box<dyn Error>> {
     return match arg {
         "help" => help(),
         "list" => list_notes(&config.notes_directory),
         "config" => edit_config(&config.config_file_path, &config.editor_command),
-        _ => create_note(&config.notes_directory, &config.editor_command, &String::from(arg)),
-    }
+        _ => create_note(
+            &config.notes_directory,
+            &config.editor_command,
+            &String::from(arg),
+        ),
+    };
 }
 
 fn match_complex_command(args: &Vec<String>, config: &Config) -> Result<(), Box<dyn Error>> {
@@ -105,7 +94,7 @@ fn match_complex_command(args: &Vec<String>, config: &Config) -> Result<(), Box<
     return match command {
         "search" => search(&config.notes_directory, search_term),
         _ => help(),
-    }
+    };
 }
 
 fn edit_config(config_file_path: &String, editor_command: &String) -> Result<(), Box<dyn Error>> {
@@ -117,39 +106,31 @@ fn edit_config(config_file_path: &String, editor_command: &String) -> Result<(),
     Ok(())
 }
 
-fn create_note(notes_dir: &String, editor_command: &String, file_name: &String) -> Result<(), Box<dyn Error>> {
+fn create_note(
+    notes_dir: &String,
+    editor_command: &String,
+    file_name: &String,
+) -> Result<(), Box<dyn Error>> {
     let file_path = format!("{}/{}.md", notes_dir, file_name);
-    let _output = Command::new(editor_command)
-        .arg(file_path)
-        .spawn()?
-        .wait();
+    let _output = Command::new(editor_command).arg(file_path).spawn()?.wait();
 
     Ok(())
 }
 
 fn search(notes_dir: &String, search_item: &String) -> Result<(), Box<dyn Error>> {
-    let output = match Command::new("rg")
-        .arg(search_item)
-        .arg(notes_dir)
-        .output() {
-            Ok(value) => value,
-            Err(e) => return Err(Box::new(e)),
-        };
+    let output = Command::new("rg").arg(search_item).arg(notes_dir).output()?;
 
     println!("{}", String::from_utf8_lossy(&output.stdout));
     Ok(())
 }
 
 fn list_notes(notes_dir: &String) -> Result<(), Box<dyn Error>> {
-    let notes = match get_notes(notes_dir) {
-        Ok(value) => value,
-        Err(e) => return Err(e),
-    };
-    
+    let notes = get_notes(notes_dir)?;
+
     for note in notes {
         print!("{}   ", note);
     }
-    
+
     Ok(())
 }
 
@@ -170,7 +151,7 @@ fn help() -> Result<(), Box<dyn Error>> {
     println!("  OR  ");
     println!("note <command> where <command> is one of:");
     println!("  list: list all existing notes in directory");
-    println!("  search <value>: search contents of all notes for <value>, including filenames"); 
+    println!("  search <value>: search contents of all notes for <value>, including filenames");
     println!("  config: edit config");
 
     Ok(())
