a44d7c6e1d255c3eb23422492287588838ed93f8
Sackbuoy <cameronkientz@proton.me>
2025-01-20 17:02:17 -0600

    cleaning up nix stuff

--- flake.nix
+++ flake.nix
@@ -1,20 +1,33 @@
 {
   description = "Rust Flake";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = nixpkgs.legacyPackages;
-    in {
-      packages = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./default.nix { };
-      });
-      devShells = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./shell.nix {};
-      });
-    };
-}
 
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      manifest = (nixpkgs.lib.importTOML ./Cargo.toml).package;
+    in {
+    defaultPackage = pkgs.rustPlatform.buildRustPackage rec {
+      pname = manifest.name;
+      version = manifest.version;
+      cargoLock.lockFile = ./Cargo.lock;
+      src = pkgs.lib.cleanSource ./.;
+    };
+
+    devShell = pkgs.mkShell { 
+      buildInputs =
+      [
+        pkgs.rust-analyzer # LSP Server
+        pkgs.rustfmt       # Formatter
+        pkgs.clippy        # Linter
+      ];
+      shellHook = ''
+        echo "Loaded Dev Environment"
+      '';
+    };
+  });
+}
