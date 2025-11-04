dc9b421c82158f337ee30eaaebb20ed02a4361b9
Sackbuoy <cameronkientz@proton.me>
2025-11-04 11:58:37 -0600

    adding support for aarch

--- flake.nix
+++ flake.nix
@@ -1,34 +1,46 @@
 {
- description = "Rust Flake";
- inputs = {
-   nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
- };
+  description = "Rust Flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
- outputs = { self, nixpkgs, flake-utils, ... }:
-   let
-     system = "x86_64-linux";
-     pkgs = nixpkgs.legacyPackages.${system};
-     manifest = (nixpkgs.lib.importTOML ./Cargo.toml).package;
-   in {
-   defaultPackage.${system} = pkgs.rustPlatform.buildRustPackage rec {
-     pname = manifest.name;
-     version = manifest.version;
-     cargoLock.lockFile = ./Cargo.lock;
-     src = pkgs.lib.cleanSource ./.;
-   };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    systems = ["x86_64-linux" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+    pkgsFor = system: nixpkgs.legacyPackages.${system};
+  in {
+    packages = forAllSystems (system: let
+      pkgs = pkgsFor system;
+      manifest = (nixpkgs.lib.importTOML ./Cargo.toml).package;
+    in {
+      default = pkgs.rustPlatform.buildRustPackage {
+        pname = manifest.name;
+        version = manifest.version;
+        cargoLock.lockFile = ./Cargo.lock;
+        src = pkgs.lib.cleanSource ./.;
+      };
+    });
 
-   devShell.${system} = pkgs.mkShell { 
-     buildInputs = [
-       pkgs.rustup
-     ];
-     shellHook = ''
-       echo "Loaded Dev Environment"
-       export RUSTUP_HOME="$PWD/.rustup"
-       export CARGO_HOME="$PWD/.cargo"
-       rustup toolchain install 1.83.0
-       rustup default 1.83.0
-       rustup component add rust-analyzer rustfmt clippy
-     '';
-   };
- };
+    devShells = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in {
+      default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rustup
+        ];
+        shellHook = ''
+          echo "Loaded Dev Environment"
+          export RUSTUP_HOME="$PWD/.rustup"
+          export CARGO_HOME="$PWD/.cargo"
+          rustup toolchain install 1.83.0
+          rustup default 1.83.0
+          rustup component add rust-analyzer rustfmt clippy
+        '';
+      };
+    });
+  };
 }
